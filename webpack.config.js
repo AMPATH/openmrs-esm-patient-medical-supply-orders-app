053afcf4932edd0c7409d88c9dd8582eda0cc2f9
drizzentic <derrick.rono@jembi.org>
2026-08-11 12:31:35 +0300

    Refactor code structure for improved readability and maintainability

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,8 @@
-module.exports = require('openmrs/default-webpack-config');
+const config = require('openmrs/default-webpack-config');
+
+// @openmrs/esm-patient-common-lib (and other @openmrs/* packages) ship raw
+// TypeScript source with no compiled dist, so they need to go through the
+// script loader too instead of being excluded like other node_modules.
+config.scriptRuleConfig.exclude = /node_modules\/(?!@openmrs)/;
+
+module.exports = config;
